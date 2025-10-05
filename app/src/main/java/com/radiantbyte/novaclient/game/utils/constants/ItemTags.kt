29ba802e8481f483ce592c5956ce97b1471bc735
RadiantByte <radiantbyte@outlook.com>
2025-10-05 04:25:19 -0700

    v1.8

---
+++ app/src/main/java/com/radiantbyte/novaclient/game/utils/constants/ItemTags.kt
@@ -0,0 +1,23 @@
+package com.radiantbyte.novaclient.game.utils.constants
+
+object ItemTags {
+    const val TAG_IS_HELMET = "minecraft:is_helmet"
+    const val TAG_IS_CHESTPLATE = "minecraft:is_chestplate"
+    const val TAG_IS_LEGGINGS = "minecraft:is_leggings"
+    const val TAG_IS_BOOTS = "minecraft:is_boots"
+    const val TAG_IS_SWORD = "minecraft:is_sword"
+    const val TAG_IS_PICKAXE = "minecraft:is_pickaxe"
+    const val TAG_IS_AXE = "minecraft:is_axe"
+    const val TAG_IS_HOE = "minecraft:is_hoe"
+    const val TAG_IS_SHOVEL = "minecraft:is_shovel"
+    const val TAG_IS_FOOD = "minecraft:is_food"
+    
+    const val TAG_LEATHER_TIER = "minecraft:leather_tier"
+    const val TAG_WOODEN_TIER = "minecraft:wooden_tier"
+    const val TAG_GOLDEN_TIER = "minecraft:golden_tier"
+    const val TAG_STONE_TIER = "minecraft:stone_tier"
+    const val TAG_CHAINMAIL_TIER = "minecraft:chainmail_tier"
+    const val TAG_IRON_TIER = "minecraft:iron_tier"
+    const val TAG_DIAMOND_TIER = "minecraft:diamond_tier"
+    const val TAG_NETHERITE_TIER = "minecraft:netherite_tier"
+}
